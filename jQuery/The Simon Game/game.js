5a4eebd9b73bdb36b7c2fddd8f729e8b7d2bbf90
modestossm <modestossm@gmail.com>
2026-04-24 18:47:28 -0300

    Added: jQuery_The Simon Game JS II

--- jQuery/The Simon Game/game.js
+++ jQuery/The Simon Game/game.js
@@ -2,13 +2,31 @@ const buttonColours = ["red", "blue", "green", "yellow"];
 const gamePattern = [];
 const userClickedPattern = [];
 
+let started = false;
+
+let level = 0;
+
+$(document).keypress(function() {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
 $(".btn").click(function() {
     let userChosenColour = $(this).attr("id");
-
     userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
 });
 
 function nextSequence() {
+    level++;
+    
+    $("#level-title").text("Level " + level);
+
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColour = buttonColours[randomNumber];
     
