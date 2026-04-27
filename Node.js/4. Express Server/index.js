3ad425585743143ae8bd7e5c4a051cfb98e93d52
modestossm <modestossm@gmail.com>
2026-04-27 20:23:28 -0300

    Added: Node.js_Server IV

--- Node.js/4. Express Server/index.js
+++ Node.js/4. Express Server/index.js
@@ -3,7 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('<h1>Hello World!</h1>');
+})
+
+app.get('/contact', (req, res) => {
+  res.send('<h1>Contact Us!</h1>');
+})
+
+app.get('/about', (req, res) => {
+  res.send('<h1>About Us!</h1>');
 })
 
 app.listen(port, () => {
