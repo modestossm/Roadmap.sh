9e15de9b023c5c4f7ca75d1caff05c759de9b4ba
modestossm <modestossm@gmail.com>
2026-04-29 14:35:28 -0300

    Added: Node.js_Secrets Project index.js

---
+++ Node.js/7. Secrets Project/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import bodyParser from "body-parser";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+let userIsAuthorised = false;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+function passwordCheck(req, res, next) {
+    const password = req.body["password"];
+    if(password === "ILoveProgramming") {
+        userIsAuthorised = true;
+    }
+    next();
+}
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", passwordCheck, (req, res) => { // Apply passwordCheck middleware specifically to this POST route
+    if(userIsAuthorised) {
+        res.sendFile(__dirname + "/public/secret.html");
+    } else {
+        userIsAuthorised = false; // It's good practice to reset this if authentication fails or for a new attempt.
+        res.sendFile(__dirname + "/public/index.html");
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});j
