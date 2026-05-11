a88b5b98122ebf8441f00f18198f38e60b715eda
modestossm <modestossm@gmail.com>
2026-05-10 21:16:19 -0300

    Added: API_Auth - index.js bugFix

--- 05. APIs/3. API Authentication/index.js
+++ 05. APIs/3. API Authentication/index.js
@@ -5,29 +5,63 @@ const app = express();
 const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com/";
 
-const yourUsername = "";
-const yourPassword = "";
-const yourAPIKey = "";
-const yourBearerToken = "";
+const yourUsername = "samssm";
+const yourPassword = "1234";
+const yourAPIKey = "cfa59178-a356-4549-a9f3-42181a80b762";
+const yourBearerToken = "e9f859dd-f05e-4e85-bd61-12036993363c";
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { content: "API Response." });
 });
 
 app.get("/noAuth", (req, res) => {
-
+  try{
+    const result = await axios.get(API_URL + "/random");
+    res.render("index.ejs", {content: JSON.stringify(result.data)})
+  } catch (error) {
+    res.status(404).send("Error:", error.message);
+  }
 });
 
 app.get("/basicAuth", (req, res) => {
-
+  try {
+    const result = await axios.get(API_URL + "/all?page=2", {
+      auth: {
+        username: yourUsername,
+        password: yourPassword,
+      },
+    });
+    res.render("index.ejs", {content: JSON.stringify(result.data)});
+  } catch (error) {
+    res.status(404).send("Error:", error.message);
+  }
 });
 
 app.get("/apiKey", (req, res) => {
-
+  try {
+    const result = await axios.get(API_URL + "/all?page=2", {
+      auth: {
+        username: yourUsername,
+        password: yourPassword,
+      },
+    });
+    res.render("index.ejs", {content: JSON.stringify(result.data)});
+  } catch (error) {
+      res.status(404).send(error.message);
+  }
 });
 
-app.get("/bearerToken", (req, res) => {
+const config = {
+  headers: { Authorization: `Bearer ${yourBearerToken}` },
+};
 
+app.get("/bearerToken", (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/secrets/2", config);
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 });
 
 app.listen(port, () => {
