ab088e736a75e07b836cd20fd9f02d1bab91f59a
modestossm <modestossm@gmail.com>
2026-05-21 21:54:20 -0300

    Added: Private API_Blog Post - POST new post

--- 6. Private API/2. Blog Post - API/index.js
+++ 6. Private API/2. Blog Post - API/index.js
@@ -51,8 +51,20 @@ app.get("/posts/:id", (req, res) => {
   res.json(post);
 });
 
-
 //POST a new post
+app.post("/posts", (req, res) => {
+  const newId = lastId += 1;
+  const post = {
+    id: newId,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    date: new Date(),
+  };
+  lastId = newId;
+  posts.push(post);
+  res.status(201).json(post);
+});
 
 //PATCH a post when you just want to update one parameter
 
