1dba6196e948f7b0b4fef18acad37c51b1b4df3c
modestossm <modestossm@gmail.com>
2026-05-27 17:35:39 -0300

    bugFix: PostgreSQL_Travel Tracker - index.ejs + index.js

--- 07. PostgreSQL/3. Travel Tracker/index.js
+++ 07. PostgreSQL/3. Travel Tracker/index.js
@@ -18,20 +18,39 @@ const db = new pg.Client({
 
 db.connect();
 
-let total_visited = 0;
-
-app.get("/", async (req, res) => {
+async function checkVisited() {
   const result = await db.query("SELECT country_code FROM visited_countries");
+
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
-  console.log(result.rows);
+
+  return countries;
+}
+
+app.get("/", async (req, res) => {
+  const countries = await checkVisited();
   res.render("index.ejs", { countries: countries, total: countries.length });
   db.end();
 });
 
 app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+  console.log(input);
+
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1", [input]
+  );
+
+  if(result.rows.length !== 0) {
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", countryCode);
+  }
+
+  res.redirect("/");
 });
 
 app.listen(port, () => {
