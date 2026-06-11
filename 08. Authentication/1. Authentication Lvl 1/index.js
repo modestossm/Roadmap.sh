ae6ffbbd3118fc2f165db5407e1526331c147499
modestossm <modestossm@gmail.com>
2026-06-11 18:17:35 -0300

    Added: Authentication_lvl 01 - new route: POST /register

--- 08. Authentication/1. Authentication Lvl 1/index.js
+++ 08. Authentication/1. Authentication Lvl 1/index.js
@@ -1,11 +1,22 @@
 import express from "express";
-import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));  
 app.use(express.static("public"));
+
+const db = new pg.Client({
+  user: process.env.DB.USER,
+  host: process.env.DB.HOST,
+  database: process.env.DB.DATABASE,
+  password: process.env.DB.PASSWORD,
+  port: process.env.DB.PORT,
+});
+
+db.connect();
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
@@ -19,9 +30,23 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {});
+app.post("/register", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-app.post("/login", async (req, res) => {});
+  const result = await db.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, password]);
+
+  console.log(result);
+  res.render("secrets.ejs");
+});
+
+app.post("/login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  console.log(username);
+  console.log(password);
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
