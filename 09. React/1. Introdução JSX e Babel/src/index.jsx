2b85672e81acc0932bedf6be8049db31183a9a7e
modestossm <modestossm@gmail.com>
2026-06-18 23:56:30 -0300

    Added: React_JSX and Babel - unordered list

--- 09. React/1. Introdução JSX e Babel/src/index.jsx
+++ 09. React/1. Introdução JSX e Babel/src/index.jsx
@@ -5,5 +5,12 @@ ReactDOM.render(
     <div>
         <h1>Hello World!</h1>
         <p>This is a paragraph.</p>
+
+        <h2>My Favourite Foods:</h2>
+        <ul>
+            <li>Bacon</li>
+            <li>Noodles</li>
+            <li>Jamon</li>
+        </ul>
     </div>,
 document.getElementById("root"));
