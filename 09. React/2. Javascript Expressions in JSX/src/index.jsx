f050a5fe2c983e69def35860ac3eb256fb8d75fa
modestossm <modestossm@gmail.com>
2026-06-19 21:26:51 -0300

    Added: React_JSX and Babel - JS Expression update

--- 09. React/2. Javascript Expressions in JSX/src/index.jsx
+++ 09. React/2. Javascript Expressions in JSX/src/index.jsx
@@ -5,10 +5,16 @@ const fName = "Angela";
 const lName = "Yu";
 const num = parseInt(Math.random() * 10);
 
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
 ReactDOM.render(
   <div>
     <h1>Hello {fName + " " + lName}!</h1>
     <p>Your lucky number is {num}</p>
+
+    <p>Created by {fName}</p>
+    <p>Copyright {year}</p>
   </div>,
   document.getElementById("root")
 );
