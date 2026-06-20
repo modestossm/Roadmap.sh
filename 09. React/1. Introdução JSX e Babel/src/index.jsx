589467de3f9ded8910f9c552955e69138d240bcb
modestossm <modestossm@gmail.com>
2026-06-19 21:51:11 -0300

    Added: React_JSX and Babel - images

--- 09. React/1. Introdução JSX e Babel/src/index.jsx
+++ 09. React/1. Introdução JSX e Babel/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(
     <div>
-        <h1>Hello World!</h1>
+        <h1 className='heading'>Hello World!</h1>
         <p>This is a paragraph.</p>
 
         <h2>My Favourite Foods:</h2>
@@ -12,5 +12,18 @@ ReactDOM.render(
             <li>Noodles</li>
             <li>Jamon</li>
         </ul>
+
+        <div className='flex'>
+            {/* <img alt="random" src={img + "?grayscale"} /> */}
+
+            <img className="food-img" alt="bacon"
+            src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190621-air-fryer-bacon-0035-landscape-pf-1567632709.jpg?crop=0.645xw:0.967xh;0.170xw,0.0204xh&resize=480:*"/>
+
+            <img className="food-img" alt="noodles"
+            src="https://www.errenskitchen.com/wp-content/uploads/2014/04/quick-and-easy-chinese-noodle-soup3-1.jpg"/>
+
+            <img className="food-img" alt="jamon"
+            src="https://images-na.ssl-images-amazon.com/images/I/71lNrnbMXsL._SL1200_.jpg"/>
+        </div>
     </div>,
 document.getElementById("root"));
