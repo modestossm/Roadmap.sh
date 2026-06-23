25f658de93dfec99819f5d899877c431a01f04bf
modestossm <modestossm@gmail.com>
2026-06-22 21:59:26 -0300

    Added: React_JSX inline style II

--- 09. React/2. Javascript Expressions in JSX/src/index.jsx
+++ 09. React/2. Javascript Expressions in JSX/src/index.jsx
@@ -8,10 +8,20 @@ const num = parseInt(Math.random() * 10);
 const currentDate = new Date();
 const year = currentDate.getFullYear();
 
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black",
+  maxWidth: "10rem",
+  backgroundColor: "coral"
+};
+
+customStyle.color = "blue";
+
 ReactDOM.render(
   <div>
-    <h1 style={{color: "orangered"}}>Hello {fName + " " + lName}!</h1>
-    <p>Your lucky number is {num}</p>
+    <h1 style={customStyle}>Hello {fName + " " + lName}!</h1>
+    <p style={{color: "orangered"}}>Your lucky number is {num}</p>
 
     <p>Created by {fName}</p>
     <p>Copyright {year}</p>
