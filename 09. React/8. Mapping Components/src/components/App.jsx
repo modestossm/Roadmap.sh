b384dc2e16b147eb4b44266408b681fdd4481323
modestossm <modestossm@gmail.com>
2026-07-06 20:49:16 -0300

    Added: React_Mapping Components - App.jsx

--- 09. React/8. Mapping Components/src/components/App.jsx
+++ 09. React/8. Mapping Components/src/components/App.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import emojipedia from "../emojipedia";
+import createCard from "./Mapping";
 
 function App() {
   return (
@@ -8,38 +10,7 @@ function App() {
       </h1>
 
       <dl className="dictionary">
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps"> 💪 </span>
-            <span>Tense Biceps</span>
-          </dt>
-          <dd>
-            “You can do that!” or “I feel strong!” Arm with tense biceps. Also
-            used in connection with doing sports, e.g. at the gym.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps"> 🙏 </span>
-            <span>Person With Folded Hands</span>
-          </dt>
-          <dd>
-            Two hands pressed together. Is currently very introverted, saying a
-            prayer, or hoping for enlightenment. Is also used as a “high five”
-            or to say thank you.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps"> 🤣 </span>
-            <span>Rolling On The Floor, Laughing</span>
-          </dt>
-          <dd>
-            This is funny! A smiley face, rolling on the floor, laughing. The
-            face is laughing boundlessly. The emoji version of “rofl“. Stands
-            for „rolling on the floor, laughing“.
-          </dd>
-        </div>
+        {emojipedia.map(createCard)}
       </dl>
     </div>
   );
