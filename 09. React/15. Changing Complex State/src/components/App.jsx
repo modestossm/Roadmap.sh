e395239e34f3cf39ef74ee1444ed2e5256b7b08d
modestossm <modestossm@gmail.com>
2026-07-13 21:41:13 -0300

    Added: React_Complex State II

--- 09. React/15. Changing Complex State/src/components/App.jsx
+++ 09. React/15. Changing Complex State/src/components/App.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 
 function App() {
+  const [fName, setfName] = React.useState("");
+  const [lName, setlName] = React.useState("");
+
+  function changeFName(e) {
+    setfName(e.target.value);
+  }
+
+  function changeLName(e) {
+    setlName(e.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {fName} {lName}</h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input name="fName" placeholder="First Name" onChange={changeFName}/>
+        <input name="lName" placeholder="Last Name" onChange={changeLName}/>
         <button>Submit</button>
       </form>
     </div>
