e81762f83b587b986f5e761c3d7622dd56885759
modestossm <modestossm@gmail.com>
2026-07-18 22:06:44 -0300

    Added: React_Queueing a Series of State Updates II

--- 09. React/18. Queueing a Series of State Updates/my-app/src/App.tsx
+++ 09. React/18. Queueing a Series of State Updates/my-app/src/App.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { useState } from 'react';
 
 export default function App() {
-    return (
-        ""
-    );
+  const [number, setNumber] = useState(0);
+
+  return (
+    <div className='div-main'>
+      <h1>{number}</h1>
+      <button className='btn' onClick={() => {
+        setNumber(number + 100);
+        setNumber(number + 99);
+        setNumber(number + 1); //apenas o ultimo valor da fila será levado em conta
+        setNumber(n => n + 1); //a função atuliza o mesmo state da variavel multiplas vezes antes do re-render, por isso soma-se ao valor anterior
+      }}>+2</button>
+    </div>
+  )
 }
