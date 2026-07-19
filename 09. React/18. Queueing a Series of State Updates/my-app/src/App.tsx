4716d6107d2e2f4b55e05107272a1af1372c236a
modestossm <modestossm@gmail.com>
2026-07-18 22:34:13 -0300

    Added: React_Queueing a Series of State Updates III

--- 09. React/18. Queueing a Series of State Updates/my-app/src/App.tsx
+++ 09. React/18. Queueing a Series of State Updates/my-app/src/App.tsx
@@ -7,11 +7,16 @@ export default function App() {
     <div className='div-main'>
       <h1>{number}</h1>
       <button className='btn' onClick={() => {
-        setNumber(number + 100);
-        setNumber(number + 99);
-        setNumber(number + 1); //apenas o ultimo valor da fila será levado em conta
-        setNumber(n => n + 1); //a função atuliza o mesmo state da variavel multiplas vezes antes do re-render, por isso soma-se ao valor anterior
+        setNumber(number + 100); 
+        setNumber(number + 99); //equivale a: setNumber(n => number + 99), por isso o valor anterior é substituído (ao invés de somado, como se esperaria)
+        setNumber(number + 1); //apenas o ultimo valor da fila será contado
+        setNumber(n => n + 1); //a função pode atulizar o mesmo state da variavel multiplas vezes antes do re-render, por isso soma-se ao valor anterior
       }}>+2</button>
     </div>
   )
 }
+
+// Naming conventions:
+// setEnabled(e => !e);
+// setLastName(ln => ln.reverse());
+// setFriendCount(fc => fc * 2);
