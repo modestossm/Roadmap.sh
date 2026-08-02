0bccb9f5aaeb73f0f7b101fa809c314af1bc510a
modestossm <modestossm@gmail.com>
2026-08-01 22:30:27 -0300

    Update: React_Reducer

--- 09. React/20. Reducer/vite-project/src/components/Reducer.tsx
+++ 09. React/20. Reducer/vite-project/src/components/Reducer.tsx
@@ -11,12 +11,21 @@ export type TaskAction =
 
 export default function tasksReducer(tasks: Task[], action: TaskAction): Task[] {
   switch (action.type) {
-    case 'added':
-      return [...tasks, { id: action.id, text: action.text, done: false }];
-    case 'changed':
-      return tasks.map(t => (t.id === action.task.id ? action.task : t));
-    case 'deleted':
-      return tasks.filter(t => t.id !== action.id);
+    case 'added': {
+        return ([
+            ...tasks,
+            { id: action.id, text: action.text, done: false }
+        ]);
+    }
+    case 'changed': {
+        return (
+            tasks.map(t => (t.id === action.task.id ? action.task : t))
+        );
+    }
+    case 'deleted': {
+        return (
+            tasks.filter(t => t.id !== action.id));
+    }
     default:
       throw new Error('Unknown action type');
   }
