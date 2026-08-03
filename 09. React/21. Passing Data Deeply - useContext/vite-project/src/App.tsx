855b88ead965203e3f02804aa18d4d2540c3548b
modestossm <modestossm@gmail.com>
2026-08-02 21:33:42 -0300

    Update: React_Passing Data Deeply - useContext II

--- 09. React/21. Passing Data Deeply - useContext/vite-project/src/App.tsx
+++ 09. React/21. Passing Data Deeply - useContext/vite-project/src/App.tsx
@@ -2,24 +2,55 @@ import Heading from './components/Heading.js';
 import Section from './components/Section.js';
 
 export default function App() {
+return (
+    <Section>
+      <Heading>My Profile</Heading>
+      <Post
+        title="Hello traveller!"
+        body="Read about my adventures."
+      />
+      <AllPosts />
+    </Section>
+  );
+}
+
+function AllPosts() {
   return (
     <Section>
-      <Heading>Title</Heading>
-      <Section>
-        <Heading>Heading</Heading>
-        <Heading>Heading</Heading>
-        <Heading>Heading</Heading>
-        <Section>
-          <Heading>Sub-heading</Heading>
-          <Heading>Sub-heading</Heading>
-          <Heading>Sub-heading</Heading>
-          <Section>
-            <Heading>Sub-sub-heading</Heading>
-            <Heading>Sub-sub-heading</Heading>
-            <Heading>Sub-sub-heading</Heading>
-          </Section>
-        </Section>
-      </Section>
+      <Heading>Posts</Heading>
+      <RecentPosts />
+    </Section>
+  );
+}
+
+function RecentPosts() {
+  return (
+    <Section>
+      <Heading>Recent Posts</Heading>
+      <Post
+        title="Flavors of Lisbon"
+        body="...those pastéis de nata!"
+      />
+      <Post
+        title="Buenos Aires in the rhythm of tango"
+        body="I loved it!"
+      />
+    </Section>
+  );
+}
+
+interface PostProps {
+  title: string;
+  body: string;
+}
+
+function Post({ title, body }: PostProps) {
+  return (
+    <Section isFancy={true}>
+      <Heading>
+        {title}
+      </Heading>
+      <p><i>{body}</i></p>
     </Section>
   );
 }
--- 09. React/21. Passing Data Deeply - useContext/vite-project/src/components/Section.tsx
+++ 09. React/21. Passing Data Deeply - useContext/vite-project/src/components/Section.tsx
@@ -2,13 +2,18 @@ import { useContext } from 'react';
 import { LevelContext } from './LevelContext.js';
 import type { PropsWithChildren } from 'react';
 
-type SectionProps = PropsWithChildren;
+interface SectionProps extends PropsWithChildren {
+  isFancy?: boolean;
+}
 
-export default function Section({ children }: SectionProps) {
+export default function Section({ children, isFancy = false }: SectionProps) {
   const level = useContext(LevelContext);
   
   return (
-     <section className="section">
+    <section className={
+      'section ' +
+      (isFancy ? 'fancy' : '')
+    }>
       <LevelContext value={level + 1}>
         {children}
       </LevelContext>
