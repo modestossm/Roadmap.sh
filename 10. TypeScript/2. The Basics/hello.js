c9ce54efb90cd2d3fe2917689eb6abbb7d68d93e
modestossm <modestossm@gmail.com>
2026-08-07 22:12:56 -0300

    Fix: TypeScript_The Basics

--- 10. TypeScript/2. The Basics/hello.js
+++ 10. TypeScript/2. The Basics/hello.js
@@ -1,5 +1,5 @@
 "use strict";
 function greet(person, date) {
-    console.log(`Hello ${person}, today is ${date.toDateString}!`);
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
 }
 greet("Brendan", new Date());
--- 10. TypeScript/2. The Basics/hello.ts
+++ 10. TypeScript/2. The Basics/hello.ts
@@ -1,5 +1,5 @@
 function greet(person: string, date: Date) {
-  console.log(`Hello ${person}, today is ${date.toDateString}!`);
+  console.log(`Hello ${person}, today is ${date.toDateString()}!`);
 }
  
-greet("Brendan");
+greet("Brendan", new Date());
