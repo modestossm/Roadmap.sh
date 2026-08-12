0af2819d0eacf4e565e23bbac9585016701ac307
modestossm <modestossm@gmail.com>
2026-08-11 21:18:00 -0300

    Update: TypeScript_The Basics - Object Types

--- 10. TypeScript/2. The Basics/hello.js
+++ 10. TypeScript/2. The Basics/hello.js
@@ -3,3 +3,9 @@ function greet(person, date) {
     console.log(`Hello ${person}, today is ${date.toDateString()}!`);
 }
 greet("Brendan", new Date());
+// Object Types
+function printCoord(pt) {
+    console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + pt.y);
+}
+printCoord({ x: 3, y: 7 });
--- 10. TypeScript/2. The Basics/hello.ts
+++ 10. TypeScript/2. The Basics/hello.ts
@@ -1,5 +1,13 @@
-function greet(person: string, date: Date) {
+function greet (person: string, date: Date) {
   console.log(`Hello ${person}, today is ${date.toDateString()}!`);
-}
+} 
  
 greet("Brendan", new Date());
+
+// Object Types
+
+function printCoord(pt: { x: number; y: number }) {
+  console.log("The coordinate's x value is " + pt.x);
+  console.log("The coordinate's y value is " + pt.y);
+}
+printCoord({ x: 3, y: 7 });
