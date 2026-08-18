0c858ee816f6e3ad68d0250fdd309b0d5007d2c5
modestossm <modestossm@gmail.com>
2026-08-18 20:34:31 -0300

    Update: TypeScript_The Basics - Discriminated unions

--- 10. TypeScript/3. Narrowing/narrowing.ts
+++ 10. TypeScript/3. Narrowing/narrowing.ts
@@ -119,3 +119,24 @@ function isFish(pet: Fish | Bird): pet is Fish {
   return (pet as Fish).swim !== undefined;
 }
 
+// Discriminated unions
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+ 
+interface Square {
+  kind: "square";
+  sideLength: number;
+}
+ 
+type Shape = Circle | Square;
+
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2; // shape: Circle
+    case "square":
+      return shape.sideLength ** 2; // shape: Square
+  }
+}
