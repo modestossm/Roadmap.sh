b18969ae64bd72f9846f8fc41d0499a16d5ec46f
modestossm <modestossm@gmail.com>
2026-08-17 21:08:49 -0300

    Update: TypeScript_The Basics - Equality narrowing

--- 10. TypeScript/3. Narrowing/narrowing.ts
+++ 10. TypeScript/3. Narrowing/narrowing.ts
@@ -33,3 +33,30 @@ function printAll(strs: string | string[] | null) {
     console.log(strs);
   }
 }
+
+// Equality narrowing
+function example(x: string | number, y: string | boolean) {
+    if (x === y) {
+        // We can now call any 'string' method on 'x' or 'y'.
+        x.toUpperCase();
+        y.toLowerCase();
+            
+    } else {
+        console.log(x);
+        console.log(y);
+  }
+}
+
+interface Container {
+  value: number | null | undefined;
+}
+ 
+function multiplyValue(container: Container, factor: number) {
+  // Remove both 'null' and 'undefined' from the type. The != operator checks whether a value is either null or undefined.
+  if (container.value != null) {
+    console.log(container.value);
+
+    // Now we can safely multiply 'container.value'.
+    container.value *= factor;
+  }
+}
