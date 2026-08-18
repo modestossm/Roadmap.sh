6f66e748ebf4d9c7c895592f66ed976c92f27340
modestossm <modestossm@gmail.com>
2026-08-18 17:25:12 -0300

    Update: TypeScript_The Basics - Control flow analysis

--- 10. TypeScript/3. Narrowing/narrowing.js
+++ 10. TypeScript/3. Narrowing/narrowing.js
@@ -73,5 +73,20 @@ function logValue(x) {
 let x = Math.random() < 0.5 ? 10 : "hello world!"; //let x: string | number
 x = 1; // let x: number
 console.log(x);
-x = true; // Error: Type 'boolean' is not assignable to type 'string | number'. 
+// x = true; // Error: Type 'boolean' is not assignable to type 'string | number'. 
 console.log(x);
+// Control flow analysis
+function example2() {
+    let x;
+    x = Math.random() < 0.5;
+    console.log(x); //let x: boolean
+    if (Math.random() < 0.5) {
+        x = "hello";
+        console.log(x); //let x: string
+    }
+    else {
+        x = 100;
+        console.log(x); //let x: number
+    }
+    return x; // let x: string | number
+}
--- 10. TypeScript/3. Narrowing/narrowing.ts
+++ 10. TypeScript/3. Narrowing/narrowing.ts
@@ -92,3 +92,21 @@ console.log(x);
 // x = true; // Error: Type 'boolean' is not assignable to type 'string | number'. 
 console.log(x);
            
+// Control flow analysis
+function example2() {
+  let x: string | number | boolean;
+ 
+  x = Math.random() < 0.5;
+ 
+  console.log(x); //let x: boolean
+ 
+  if (Math.random() < 0.5) {
+    x = "hello";
+    console.log(x); //let x: string
+  } else {
+    x = 100;
+    console.log(x); //let x: number
+  }
+ 
+  return x; // let x: string | number
+}
