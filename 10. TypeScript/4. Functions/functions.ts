276dbf8d095226bc965a595799601a671240ef19
modestossm <modestossm@gmail.com>
2026-08-21 22:01:48 -0300

    Update: TypeScript_Functions - Call Signatures

--- 10. TypeScript/4. Functions/functions.ts
+++ 10. TypeScript/4. Functions/functions.ts
@@ -9,3 +9,19 @@ function printToConsole(s: string) {
 }
  
 greeter(printToConsole);
+
+// Call Signatures
+type DescribableFunction = {
+  description: string; 
+  (someArg: number): boolean; 
+};
+function doSomething(fn: DescribableFunction) {
+  console.log(fn.description + " returned " + fn(6));
+}
+ 
+function myFunc(someArg: number) {
+  return someArg > 3;
+}
+myFunc.description = "default description";
+ 
+doSomething(myFunc);
