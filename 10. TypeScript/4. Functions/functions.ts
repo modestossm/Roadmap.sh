e95bfb52f7044eca25e9c3ffe32334061d54b359
modestossm <modestossm@gmail.com>
2026-08-27 18:33:24 -0300

    Update: TypeScript_Functions - Function Overloads

--- 10. TypeScript/4. Functions/functions.ts
+++ 10. TypeScript/4. Functions/functions.ts
@@ -124,3 +124,17 @@ myForEach([1, 2, 3], (a, i) => {
 // Error: 'i' is possibly 'undefined'.
 // Rule: When writing a function type for a callback, never write an optional parameter unless you intend to call the function without passing that argument
 });
+
+// Function Overloads
+function makeDate(timestamp: number): Date;
+function makeDate(m: number, d: number, y: number): Date;
+function makeDate(mOrTimestamp: number, d?: number, y?: number): Date {
+  if (d !== undefined && y !== undefined) {
+    return new Date(y, mOrTimestamp, d);
+  } else {
+    return new Date(mOrTimestamp);
+  }
+}
+const d1 = makeDate(12345678);
+const d2 = makeDate(5, 5, 5);
+// const d3 = makeDate(1, 3); // Error: No overload expects 2 arguments, but overloads do exist that expect either 1 or 3 arguments.
