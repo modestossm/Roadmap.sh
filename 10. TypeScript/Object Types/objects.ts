38e7370d0c6779dfa581cb198216063529d9fe2d
modestossm <modestossm@gmail.com>
2026-08-28 20:49:09 -0300

    Update: TypeScript_Objects Types - readonly Properties

--- 10. TypeScript/Object Types/objects.ts
+++ 10. TypeScript/Object Types/objects.ts
@@ -22,3 +22,17 @@ paintShape({ shape });
 paintShape({ shape, xPos: 100 });
 paintShape({ shape, yPos: 100 });
 paintShape({ shape, xPos: 100, yPos: 100 });
+
+
+// readonly Properties
+interface SomeType {
+  readonly prop: string;
+}
+ 
+function doSomething(obj: SomeType) {
+  // We can read from 'obj.prop'.
+  console.log(`prop has the value '${obj.prop}'.`);
+ 
+  // But we can't re-assign it.
+  // obj.prop = "hello"; // Error: Cannot assign to 'prop' because it is a read-only property.
+}
