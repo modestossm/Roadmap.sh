cb06f1e0d52f43e32390d9419b7518b35d5003fc
modestossm <modestossm@gmail.com>
2026-09-04 22:31:50 -0300

    Update: Testes_Testes Unitários - Jest --coverage

--- 12. Testes/1. Teste Unitário - Jest/soma.js
+++ 12. Testes/1. Teste Unitário - Jest/soma.js
@@ -2,4 +2,8 @@ function soma(a, b) {
     return a + b;
 }
 
-module.exports = soma
+function soma3(a, b, c) {
+    return (a + b + c);
+}
+
+module.exports = { soma, soma3 };
--- 12. Testes/1. Teste Unitário - Jest/test/soma.test.js
+++ 12. Testes/1. Teste Unitário - Jest/test/soma.test.js
@@ -1,5 +1,9 @@
-const soma = require("../soma");
+const { soma, soma3 } = require("../soma");
 
 test("soma 1 + 2 o resultado tem que se 3", function() {
     expect(soma(1,2)).toBe(3);
+})
+
+test("soma 1 + 2 + 3 o resultado tem que se 6", function() {
+    expect(soma3(1,2,3)).toBe(6);
 })
