c814f8ea5e20d09bcc8a1360257ea81f3d66d382
modestossm <modestossm@gmail.com>
2026-09-12 22:13:10 -0300

    Update: React_Escape Hatches - Referencing Values with Refs II

--- 09. React/24. Escape Hatches/react/src/App.tsx
+++ 09. React/24. Escape Hatches/react/src/App.tsx
@@ -1,9 +1,11 @@
-import Counter from "./components/Referencing Values with Refs";
+import Counter from "./components/Referencing Values with Refs I";
+import Stopwatch from "./components/Referencing Values with Refs II";
 
 function App() {
   return (
     <>
       <Counter />
+      <Stopwatch />
     </>
   );
 }
