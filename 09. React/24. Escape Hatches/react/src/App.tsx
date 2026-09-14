0bf36f6045b006b8baa1de8ee816f09eb7bb1a26
modestossm <modestossm@gmail.com>
2026-09-13 22:12:06 -0300

    Update: React_Escape Hatches - Manipulating the DOM with Refs

--- 09. React/24. Escape Hatches/react/src/App.tsx
+++ 09. React/24. Escape Hatches/react/src/App.tsx
@@ -1,11 +1,13 @@
 import Counter from "./components/Referencing Values with Refs I";
 import Stopwatch from "./components/Referencing Values with Refs II";
+import Form from "./components/Manipulating the DOM with Refs";
 
 function App() {
   return (
     <>
       <Counter />
       <Stopwatch />
+      <Form />
     </>
   );
 }
