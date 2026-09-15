9a73be2a48751331e942563d26607dce1d0dad5a
modestossm <modestossm@gmail.com>
2026-09-15 20:08:50 -0300

    Update: React_Escape Hatches - Effect firing twice in dev (w/ comments)

--- 09. React/24. Escape Hatches/react/src/components/useEffect firing twice in dev.tsx
+++ 09. React/24. Escape Hatches/react/src/components/useEffect firing twice in dev.tsx
@@ -12,8 +12,10 @@ function Playground() {
         const timeoutId = setTimeout(onTimeout, 3000);
 
         return () => {
-             console.log('🟡 Cancel "' + text + '" log');
-            clearTimeout(timeoutId);
+            console.log('🟡 Cancel "' + text + '" log');
+            // React always cleans up the previous render’s Effect before the next render’s Effect. 
+            // This is why even if you type into the input fast, there is at most one timeout scheduled at a time
+            clearTimeout(timeoutId); 
         }
     });
 
@@ -32,6 +34,7 @@ export default function AppPlayground() {
   return (
     <div >
       <button onClick={() => setShow(!show)} className="ms-20 mt-20 px-4 py-2 rounded text-white bg-blue-500">
+        {/* Notice how unmounting cleans up the last render’s Effect. Here, it clears the last timeout before it has a chance to fire */}
         {show ? 'Unmount' : 'Mount'} the component
       </button>
       {show && <br />}
