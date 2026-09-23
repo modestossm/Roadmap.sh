3885277eeb35bddab36766225607481d443478ea
modestossm <modestossm@gmail.com>
2026-09-22 22:23:54 -0300

    Update: TypeScript_Type Manipulation - Generics XI

--- 10. TypeScript/6. Type Manipulation/generics.ts
+++ 10. TypeScript/6. Type Manipulation/generics.ts
@@ -120,3 +120,19 @@ function createInstance<A extends Animal>(c: new () => A): A {
  
 createInstance(Lion).keeper.nametag;
 createInstance(Bee).keeper.hasMask;
+
+// 7. Generic Parameter Defaults
+
+interface Container<T, U> {
+  element: T;
+  children: U;
+}
+
+declare function create<T extends HTMLElement = HTMLDivElement, U extends HTMLElement[] = T[]>(
+  element?: T,
+  children?: U
+): Container<T, U>;
+ 
+const div = create(); // const div: Container<HTMLDivElement, HTMLDivElement[]>
+ 
+const p = create(new HTMLParagraphElement()); // const p: Container<HTMLParagraphElement, HTMLParagraphElement[]>
