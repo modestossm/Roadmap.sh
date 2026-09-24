490f3bfc1ca82e4e17b7585cd1889d99f0fec1e2
modestossm <modestossm@gmail.com>
2026-09-23 22:14:12 -0300

    Update: TypeScript_Type Manipulation - Indexed Access Types

---
+++ 10. TypeScript/6. Type Manipulation/Indexed Access Types.ts
@@ -0,0 +1,13 @@
+// Indexed Access Types
+
+type Person = { age: number; name: string; alive: boolean };
+
+// Examples:
+type Age = Person["age"]; // type Age = number
+
+type I1 = Person["age" | "name"]; // type I1 = string | number
+ 
+type I2 = Person[keyof Person]; // type I2 = string | number | boolean
+ 
+type AliveOrName = "alive" | "name";
+type I3 = Person[AliveOrName]; // type I3 = string | boolean
