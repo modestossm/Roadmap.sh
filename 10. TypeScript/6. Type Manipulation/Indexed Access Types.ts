b8bc0c3407848d097424fb580287cc70d99b0292
modestossm <modestossm@gmail.com>
2026-09-23 22:16:47 -0300

    Update: TypeScript_Type Manipulation - Indexed Access Types II

--- 10. TypeScript/6. Type Manipulation/Indexed Access Types.ts
+++ 10. TypeScript/6. Type Manipulation/Indexed Access Types.ts
@@ -11,3 +11,20 @@ type I2 = Person[keyof Person]; // type I2 = string | number | boolean
  
 type AliveOrName = "alive" | "name";
 type I3 = Person[AliveOrName]; // type I3 = string | boolean
+
+// type I1 = Person["alve"]; // Error: Property 'alve' does not exist on type 'Person'.
+
+// Another example of indexing with an arbitrary type is using number to get the type of an array’s elements:
+
+const MyArray = [
+  { name: "Alice", age: 15 },
+  { name: "Bob", age: 23 },
+  { name: "Eve", age: 38 },
+];
+ 
+type Person1 = typeof MyArray[number]; // type Person = { name: string, age: number }
+type Age1 = typeof MyArray[number]["age"]; // type Age = number
+
+// Or
+
+type Age2 = Person["age"]; //type Age2 = number
