5507081234c6ef5681d8bccc14f6995b82db0b20
modestossm <modestossm@gmail.com>
2026-09-24 20:40:57 -0300

    Update: TypeScript_Type Manipulation - Mapped Types II

--- 10. TypeScript/6. Type Manipulation/Mapped Types.ts
+++ 10. TypeScript/6. Type Manipulation/Mapped Types.ts
@@ -10,4 +10,34 @@ type Features = {
   newUserProfile: () => void;
 };
  
-type FeatureOptions = OptionsFlags<Features>; // type FeatureOptions = { darkMode: boolean, newUserProfile: boolean }z
+type FeatureOptions = OptionsFlags<Features>; // type FeatureOptions = { darkMode: boolean, newUserProfile: boolean }
+
+
+// 1. Mapping Modifiers
+// There are two additional modifiers which can be applied during mapping: readonly and ? which affect mutability and optionality respectively.
+// You can remove or add these modifiers by prefixing with - or +. If you don’t add a prefix, then + is assumed.
+
+// Removes 'readonly' attributes from a type's properties:
+type CreateMutable<Type> = {
+  -readonly [Property in keyof Type]: Type[Property];
+};
+ 
+type LockedAccount = {
+  readonly id: string;
+  readonly name: string;
+};
+ 
+type UnlockedAccount = CreateMutable<LockedAccount>; // type UnlockedAccount = { id: string, name: string }
+
+// Removes 'optional' attributes from a type's properties:
+type Concrete<Type> = {
+  [Property in keyof Type]-?: Type[Property];
+};
+ 
+type MaybeUser = {
+  id: string;
+  name?: string;
+  age?: number;
+};
+ 
+type User = Concrete<MaybeUser>; // type User = { id: string, name: string, age: number }
