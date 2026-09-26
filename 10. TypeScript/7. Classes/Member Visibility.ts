122ef890dab4e09e3843499adee84d73b9ea2373
modestossm <modestossm@gmail.com>
2026-09-25 21:31:06 -0300

    Update: TypeScript_Classes - Class Heritage VI

--- 10. TypeScript/7. Classes/Member Visibility.ts
+++ 10. TypeScript/7. Classes/Member Visibility.ts
@@ -13,3 +13,40 @@ class Greeter3 {
 const g3 = new Greeter3();
 g3.greet();
 
+
+// 3.1 protected
+// protected members are only visible to subclasses of the class they’re declared in:
+
+class Greeter4 {
+  public greet() {
+    console.log("Hello, " + this.getName());
+  }
+  protected getName() {
+    return "hi";
+  }
+}
+ 
+class SpecialGreeter extends Greeter4 {
+  public howdy() {
+    // OK to access protected member here
+    console.log("Howdy, " + this.getName());
+  }
+}
+const g4 = new SpecialGreeter();
+g4.greet(); // OK
+// g4.getName(); // Error: Property 'getName' is protected and only accessible within class 'Greeter' and its subclasses.
+
+// Exposure of protected members:
+// Derived classes need to follow their base class contracts, 
+// but may choose to expose a subtype of base class with more capabilities. This includes making protected members public:
+
+class Base5 {
+  protected m = 10;
+}
+class Derived5 extends Base5 {
+  // No modifier, so default is 'public'
+  public m = 15;
+}
+const d5 = new Derived5();
+console.log(d5.m); // OK
+
